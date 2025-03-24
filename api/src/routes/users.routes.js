6eec4c534d136e2305033c95b3ffecef86ad94d1
Yuri Sousa <ysousa.dev@gmail.com>
2025-03-24 14:43:26 -0300

    feat: :sparkles: adiciona validação de user, pra fins de segurança

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
-
+const UsersValidatedController = require("../controllers/UserValidatedController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 const usersRoutes = Router();
 
 const usersController = new UsersController();
+const usersValidatedController = new UsersValidatedController();
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.put("/validated", ensureAuthenticated, usersValidatedController.index);
 
 module.exports = usersRoutes;
--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import { useAuth } from "../hooks/auth";
+import { ROLES } from '../utils/roles';
+import { api } from '../services/api';
 
 import { AuthRoutes } from './auth.routes';
 import { AdminRoutes } from './admin.routes';
 import { SalesRoutes } from './sales.routes';
 import { CustomerRoutes } from './customer.routes';
-import { ROLES } from '../utils/roles';
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
 
   function acessRoutes() {
     switch (user.role) {
@@ -21,6 +23,15 @@ export function Routes() {
         return <CustomerRoutes />;
     }
   }
+
+  useEffect(() => {
+    api.get("/users/validated").catch((error) => {
+      if (error.response.status === 401) {
+        console.log(error);
+        signOut();
+      }
+    });
+  }, []);
 
   return (
     <BrowserRouter>
