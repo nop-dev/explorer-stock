ae5ce2066178a3283154fbbead28d42cf39269d4
Yuri Sousa <ysousa.dev@gmail.com>
2025-03-24 10:53:55 -0300

    feat: :sparkles: filtra as features de home pra cada tipo de user

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,18 +1,40 @@
-import { FiTruck, FiTag, FiShoppingCart } from 'react-icons/fi';
+import { FiShoppingCart, FiTag, FiTruck } from 'react-icons/fi';
 
-import { Container } from "./styles";
 import { Feature } from '../../components/Feature';
 import { Header } from '../../components/Header';
+import { Container } from "./styles";
+
+import { useAuth } from '../../hooks/auth';
+import { ROLES } from '../../utils/roles';
 
 export function Home() {
+  const { user } = useAuth();
   return (
     <Container>
       <Header />
 
       <main>
-        <Feature title="Produto" icon={FiTag} to="/product" />
-        <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
-        <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+        {user.role === ROLES.ADMIN && (
+          <>
+            <Feature title="Produto" icon={FiTag} to="/product" />
+            <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
+            <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+          </>
+        )}
+
+        {user.role === ROLES.SALES && (
+          <>
+            <Feature title="Produto" icon={FiTag} to="/product" />
+            <Feature title="Vendas" icon={FiShoppingCart} to="/sales" />
+          </>
+        )}
+
+        {user.role === ROLES.CUSTOMER && (
+          <>
+            <Feature title="Produtos" icon={FiTag} to="/products" />
+            <Feature title="Meus Pedidos" icon={FiShoppingCart} to="/my-orders" />
+          </>
+        )}
       </main>
     </Container>
   )
